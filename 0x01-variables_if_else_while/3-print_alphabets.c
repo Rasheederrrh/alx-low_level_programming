dc48be360446fb02d4e37debab6589ec082cca15
rasheederrrh <abdulsalamrasheedaholaitan@gmail.com>
2022-09-10 15:11:04 -0700

    Task 3-print_alphabets.c3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,48 +6,25 @@
  */
 int main(void)
 
-
-
 {
-
 	char lower_case; /*Decarling statement*/
-
 	char upper_case;
 
-
-
 	/* for lower case*/
-
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-
 	{
-
 		putchar(lower_case);
-
 	}
-
-
 
 	/*for upper case*/
-
 	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-
 	{
-
 		putchar(upper_case);
-
 	}
 
-
-
 	/*new line*/
-
 	putchar ('\n');
 
-
-
 	return (0);
-
-
 
 }
