29072d5fdc06a03147af56827f1a14177babf06c
rasheederrrh <abdulsalamrasheedaholaitan@gmail.com>
2022-09-13 18:36:03 -0700

    Another fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,31 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: nothing, return void
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
-
 {
-int i; /*Declaring statements*/
-char string[9] = "main.h"; /*Arrays*/
+	_putchar('_');
 
-for (i = 0 ; i < 9 ; i++) /*Start for*/
-{
-_putchar(string[i]);/*display word*/
-} /*End for*/
+	_putchar('p');
 
-_putchar('\n');
+	_putchar('u');
 
-return (0);
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+        _putchar('a');
+
+        _putchar('r');
+	_putchar('\n');
+
+        return (0);
 
 }
+
+
